b43e7d184eaf4ca73418823c3cb749d54e672ef6
Daniel Fortunato <daniel.f.castro@hotmail.com>
2022-04-06 23:58:37 -0300

    Aplivativo Hello World (1.0)

---
+++ Teste/app/src/main/java/com/example/atividade_calculadoraaposentadoria/home.kt
@@ -0,0 +1,37 @@
+package com.example.atividade_calculadoraaposentadoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val planeta = findViewById<ImageView>(R.id.img_planeta)
+        val barra = findViewById<ImageView>(R.id.img_barra)
+        val hello = findViewById<ImageView>(R.id.img_hello)
+        val btn_neon = findViewById<Button>(R.id.btn_neon)
+        var flag = false
+
+        btn_neon.setOnClickListener{
+
+            if(flag){
+                planeta.setImageResource(R.drawable.planeta)
+                barra.setImageResource(R.drawable.barra)
+                hello.setImageResource(R.drawable.hello_world2)
+
+                flag = false
+            }else{
+                planeta.setImageResource(R.drawable.neon)
+                barra.setImageResource(R.drawable.barra_neon)
+                hello.setImageResource(R.drawable.hello_world_neon)
+
+                flag = true
+            }
+
+        }
+    }
+}
